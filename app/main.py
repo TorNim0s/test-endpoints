0faa843039eadd0dec0f10c0fd3114002c9477ca
Eldad Tsemach <t-etsemach@microsoft.com>
2023-08-19 23:36:20 +0300

    save

--- app/main.py
+++ app/main.py
@@ -17,11 +17,28 @@ import logging
 from flask import Flask
 from flask import request
 from flask import Response
-from app import vuln
+import os
+import connexion
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+vuln_app = connexion.App(__name__, specification_dir='./openapi_specs')
 
-@app.route('/home', methods=['GET'])
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(vuln_app.root_path, 'database/database.db')
+vuln_app.app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+vuln_app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+vuln_app.app.config['SECRET_KEY'] = 'random'
+# start the db
+db = SQLAlchemy(vuln_app.app)
+
+vuln = int(os.getenv('vulnerable', 1))
+# vuln=1
+# token alive for how many seconds?
+alive = int(os.getenv('tokentimetolive', 60))
+
+# app = Flask(__name__)
+
+@vuln.route('/home', methods=['GET'])
 def home():
     response_text = '{ "message": "VAmPI the Vulnerable API", "help": "VAmPI is a vulnerable on purpose API. It was ' \
                     'created in order to evaluate the efficiency of third party tools in identifying vulnerabilities ' \
@@ -30,4 +47,5 @@ def home():
     return response
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    vuln_app.run(host='127.0.0.1', port=8080, debug=True)
+    # app.run(host='127.0.0.1', port=8080, debug=True)
